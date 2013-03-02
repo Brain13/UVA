6bf2beb4a15aee26a130f5ce01ccaa032d6eca1e
Brian <bjk33@zips.uakron.edu>
2013-03-01 20:54:25 -0500

    unifnished vito

---
+++ Vito.java
@@ -0,0 +1,31 @@
+// 10041 Vito's Family
+import java.util.*;
+public class Vito {
+    public static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+        int cases = input.nextInt();
+        
+        for (int q = 0; q < cases; ++q) {
+        
+            int houses = input.nextInt();
+            int[] addresses = new int[houses];
+            
+            for (int i = 0; i < houses; ++i) {
+                addresses[i] = input.nextInt();
+            }
+        
+            int median = (addresses[houses / 2] + addresses[(houses - 1) / 2]) / 2;
+            int median2 = (addresses[houses / 2] + addresses[(houses - 1) / 2]) / 2 + 1;
+            int distance = 0;
+            int distance2 = 0;
+            for (int i = 0; i < houses; ++i) {
+                distance += Math.abs(addresses[i] - median);
+                distance2 += Math.abs(addresses[i] - median2);
+            }
+            
+            System.out.println(Math.min(distance, distance2));
+        
+        }
+    }
+}
