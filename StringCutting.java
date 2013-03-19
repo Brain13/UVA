37a92fa2c907d56bca0d7fbcbaef06d048b4dade
Brian <bjk33@zips.uakron.edu>
2013-03-19 16:13:06 -0400

    first drafts

---
+++ StringCutting.java
@@ -0,0 +1,22 @@
+// 1215 String Cutting
+import java.util.*;
+public class StringCutting {
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		int cases = input.nextInt();
+		for (int q = 0; q < cases; ++q) {
+		
+			int num = input.nextInt();
+			int[] cuts = new int[num];
+			for (int i = 0; i < num; ++i)
+				cuts[i] = input.nextInt();
+				
+			String text = input.next();
+			
+			for (int i = 0; i < num; ++i) {
+			
+			}		
+		}
+	}
+}
---
+++ Suspects.java
@@ -0,0 +1,74 @@
+// 1197 The Suspects
+import java.util.*;
+public class Suspects {
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		while (true) {
+		
+			int n = input.nextInt();
+			int m = input.nextInt();
+			if (n + m == 0) return;
+			
+			Student[] st = new Student[n];
+			Group[] gp = new Group[m];
+			
+			// read in data
+			for (int i = 0; i < m; ++i) {
+				gp[i] = new Group();
+				gp[i].students = new ArrayList<Integer>();
+				int members = input.nextInt();
+				for (int j = 0; j < members; ++j) {
+					int id = input.nextInt();
+					if (st[id] == null) {
+						st[id] = new Student();
+						st[id].groups = new ArrayList<Integer>();
+					}
+					st[id].groups.add(i);
+					gp[i].students.add(id);
+				}
+			}
+			
+			// find suspects
+			Stack<Group> stack = new Stack<Group>();
+			Student initial = st[0];
+			initial.suspect = true;
+			for (int i = 0; i < initial.groups.size(); ++i) {
+				int gid = initial.groups.get(i);
+				stack.push(gp[gid]);
+				gp[gid].tracked = true;
+			}
+			while (!stack.empty()) {
+				Group g = stack.pop();
+				for (int i = 0; i < g.students.size(); ++i) {
+					int sid = g.students.get(i);
+					st[sid].suspect = true;
+					for (int j = 0; j < st[sid].groups.size(); ++j) {
+						int gid = st[sid].groups.get(j);
+						if (!gp[gid].tracked) {
+							stack.push(gp[gid]);
+						}
+					}
+				}
+			}
+			
+			// output suspects
+			int sus = 0;
+			for (int i = 0; i < n; ++i) {
+				if (st[i].suspect)
+					++sus;
+			}
+			System.out.println(sus);
+		}
+	}
+	
+	private static class Student {
+		public ArrayList<Integer> groups;
+		public boolean suspect;
+	}
+	
+	private static class Group {
+		public ArrayList<Integer> students;
+		public boolean tracked;
+	}
+}
