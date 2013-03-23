eff7a9003d89ee6f7324bf9b7dc20d29ade8dace
Brian <bjk33@zips.uakron.edu>
2013-03-22 20:23:21 -0400

    fixed circle, now correct
    law of cosines gave me an angle, law of sines gave me a diameter

--- Circle.java
+++ Circle.java
@@ -14,12 +14,13 @@ public class Circle {
             double x3 = input.nextDouble();
             double y3 = input.nextDouble();
             
-            double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) / 2;
+            double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
             double length2 = Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
-            double angle = Math.acos(length1 / length2);
-            double radius = length1 / Math.cos(angle / 2);
+            double length3 = Math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+            double angle = Math.acos((length1 * length1 - length2 * length2 - length3 * length3) / (2 * length2 * length3));
+            double diameter = length1 / Math.sin(angle);
             
-            double cir = 2 * Math.PI * radius;
+            double cir = Math.PI * diameter;
             
             System.out.printf("%.2f\n", cir);
         
