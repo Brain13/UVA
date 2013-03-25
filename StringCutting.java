9ebc09b680d815302f9175092fb1c34f76ce78ef
Brian <bjk33@zips.uakron.edu>
2013-03-25 11:32:52 -0400

    solution to string cutting

--- StringCutting.java
+++ StringCutting.java
@@ -13,10 +13,47 @@ public class StringCutting {
 				cuts[i] = input.nextInt();
 				
 			String text = input.next();
-			
+			ArrayList<String> strings = new ArrayList<String>();
+            strings.add(text);
+            int totalCut = 0;
+            // make a cut
 			for (int i = 0; i < num; ++i) {
-			
-			}		
+                int index = -1;
+                int total = 0;
+                
+                // find the cut
+                for (int j = 0; j < strings.size(); ++j) {
+                    if (cuts[i] < total + strings.get(j).length()) {
+                        index = j;
+                        break;
+                    }
+                    total += strings.get(j).length();
+                }
+                
+                String cur = strings.get(index);
+                String s1 = cur.substring(0, cuts[i] - total);
+                String s2 = cur.substring(cuts[i] - total);
+
+                boolean[] alpha1 = new boolean[26];
+                boolean[] alpha2 = new boolean[26];
+                for (int j = 0; j < s1.length(); ++j) {
+                    alpha1[s1.charAt(j) - 'a'] = true;
+                }
+                for (int j = 0; j < s2.length(); ++j) {
+                    alpha2[s2.charAt(j) - 'a'] = true;
+                }
+                for (int j = 0; j < 26; ++j) {
+                    if ((alpha1[j] && !alpha2[j]) || (!alpha1[j] && alpha2[j])) {
+                        ++totalCut;
+                    }
+                }
+                
+                // put the string back
+                strings.set(index, s1);
+                strings.add(index + 1, s2);
+			}
+            
+            System.out.println(totalCut);
 		}
 	}
 }
